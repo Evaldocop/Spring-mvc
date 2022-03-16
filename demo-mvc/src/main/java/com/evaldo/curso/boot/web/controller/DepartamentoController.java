829588ff6ce57383d6989bce1d05e10d53f3b73c
Evaldo <evaldocop@yahoo.com.br>
2022-03-16 17:07:53 -0300

    insert and list departament

--- demo-mvc/src/main/java/com/evaldo/curso/boot/web/controller/DepartamentoController.java
+++ demo-mvc/src/main/java/com/evaldo/curso/boot/web/controller/DepartamentoController.java
@@ -1,22 +1,39 @@
 package com.evaldo.curso.boot.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.evaldo.curso.boot.domain.Departamento;
+import com.evaldo.curso.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
+	
+	@Autowired
+	private DepartamentoService deparamentoService;
 
 	@GetMapping("/cadastrar")
-	public String Cadastro() {
+	public String Cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("departamentos", deparamentoService.buscarTodos());
 		return "/departamento/lista";
 	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento){
+		deparamentoService.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
+	
+	
 
 }
