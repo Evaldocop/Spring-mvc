02dc7f7daf91b20cb471c7a3aaa754dcfe4c8890
Evaldo <evaldocop@yahoo.com.br>
2022-06-27 10:07:45 -0300

    alteração do pom, para ao do professos

--- demo-mvc/src/test/java/com/evaldo/curso/boot/DemoMvcApplicationTests.java
+++ demo-mvc/src/test/java/com/evaldo/curso/boot/DemoMvcApplicationTests.java
@@ -1,13 +1,12 @@
 package com.evaldo.curso.boot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoMvcApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class DemoMvcApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
